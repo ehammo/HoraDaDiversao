7ba5c7333d21ded20165c79f522a7ec38ed8ca8f
ehammo <ehammo@cin.ufpe.br>
2016-07-15 13:58:08 -0300

    User table, tentaiva de create

--- tables/user.js
+++ tables/user.js
@@ -1,6 +1,35 @@
 module.exports = function(tables) {
 	
+	var User = tables.User;
 	
+	//create
+	
+	createUser(name, email,pass,address,phone,kids,orders){
+		
+		User.find({
+			where: {email : email}
+		})
+		.then(function(user){
+			if (user) {
+				res.status(500);
+					res.send("Erro! Já existe um usuário cadastrado com o email " + email);
+					console.log("error! already has user with email " + email);
+			} else{
+				User.create({
+					name: name,
+					email: email,
+					password: pass,
+					address:address,
+					phone: phone
+				}).then(function (user2) {
+					user2.setKids(kids);
+					user2.setHistory(orders);
+					console.log("created user: " + JSON.stringify(user2.dataValues) );
+				});
+			}
+		});
+		
+	}
 	
 	
 	
